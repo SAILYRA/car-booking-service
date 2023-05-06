33c45b10db3d1e57632e47ea257743a2c64e252b
shabbirislam <shabbirislam801@gmail.com>
2023-05-06 14:41:49 +0200

    first commit

---
+++ src/main/java/com/example/carbookingservice/CarBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.carbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarBookingServiceApplication.class, args);
+    }
+
+}
